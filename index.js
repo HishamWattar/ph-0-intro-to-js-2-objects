507552c0e879db7274b9089a401d618ae599ed7b
hisham <h.wattar@netro.xyz>
2023-03-20 23:47:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+// Create employee object
+const employee = {
+  name: 'John Doe',
+  streetAddress: '123 Main St',
+};
+
+// Update employee object with a new key-value pair
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return { ...employee, [key]: value };
+}
+
+// Destructively update employee object with a new key-value pair
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+// Delete a key-value pair from the employee object and return a new object
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+// Destructively delete a key-value pair from the employee object
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
